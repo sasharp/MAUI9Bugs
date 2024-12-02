ad3ef9f259263b103a143c4c655adc6d9b8553ad
Alexander Filuk <af@am-its.com>
2024-12-02 16:07:35 +0100

    Add project files.

---
+++ MAUI9Bugs/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace MAUI9Bugs
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
